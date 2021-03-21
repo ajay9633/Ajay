794dd0f4fc4223cda8c051dfe86f6fcf3a09199c
ajay9633 <58688470+ajay9633@users.noreply.github.com>
2021-03-21 08:57:59 +0530

    Update system_details.py

--- system_details.py
+++ system_details.py
@@ -1,2 +1,12 @@
 import wmi
+
 sys = wmi.WMI()
+my_sys = c.Win32_ComputerSystem()[0]
+print(f"Manufacturer: {my_sys.Manufacturer}")
+print(f"Model: {my_sys.Model}")
+print(f"Name: {my_sys.Name}")
+print(f"ProcessorNumber: {my_sys.NumberofProcessors}")
+print(f"Type: {my_sys.SystemType}")
+print(f"Family: {my_sys.SystemFamily}")
+
+
